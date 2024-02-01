0e6a896dd2a54b7cd015853108a437767ca4ecfd
Pranav Soni <pranavsoni1101@gmail.com>
2024-02-01 18:21:24 +0530

    Added delete route to pdfmulter

--- routes/pdfMulter.js
+++ routes/pdfMulter.js
@@ -7,6 +7,33 @@ const PDF = require('../models/pdfMulterModel');
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
+// Route to retrieve and view all PDF files
+router.get('/getAll', async (req, res) => {
+  try {
+    const pdfs = await PDF.find({});
+    res.json(pdfs);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+
+// To retrive pdf
+router.get('/findOne/:id', async (req, res) => {
+  try {
+    const pdf = await PDF.findById(req.params.id);
+    if (!pdf) {
+      return res.status(404).json({ message: 'PDF not found' });
+    }
+    res.setHeader('Content-Type', 'application/pdf');
+    res.send(pdf.data);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
 // Route to handle file uploads
 router.post('/upload', upload.single('pdf'), async (req, res) => {
   try {
@@ -22,6 +49,20 @@ router.post('/upload', upload.single('pdf'), async (req, res) => {
     await newPDF.save();
 
     return res.status(201).json({ message: 'File uploaded successfully' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+router.delete('/pdf/:id', async (req, res) => {
+  try {
+    const pdf = await PDF.findById(req.params.id);
+    if (!pdf) {
+      return res.status(404).json({ message: 'PDF not found' });
+    }
+    await pdf.remove();
+    return res.status(200).json({ message: 'PDF deleted successfully' });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: 'Internal Server Error' });
