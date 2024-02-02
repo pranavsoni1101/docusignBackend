f46a1e7b01cfcbb970d8eacce3f47aad8f09c70c
Pranav Soni <pranavsoni1101@gmail.com>
2024-02-02 15:32:06 +0530

    Added logic to fetch one, fetch all signatures, and to delete

--- routes/signature.js
+++ routes/signature.js
@@ -8,6 +8,32 @@ const Signature = require('../models/SignatureModel');
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
+// Route to retrieve and view all Signature images
+router.get('/getAll', async (req, res) => {
+    try {
+      const signatures = await Signature.find({});
+      res.json(signatures);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Internal Server Error' });
+    }
+  });
+
+// To retrive one signature
+router.get('/findOne/:id', async (req, res) => {
+    try {
+      const signature = await Signature.findById(req.params.id);
+      if (!signature) {
+        return res.status(404).json({ message: 'Signature not found' });
+      }
+      res.setHeader('Content-Type', 'image/png');
+      res.send(signature.data);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Internal Server Error' });
+    }
+  });
+  
 // Route to handle signature upload
 router.post('/upload', upload.single('signature'), async (req, res) => {
     try {
@@ -23,6 +49,21 @@ router.post('/upload', upload.single('signature'), async (req, res) => {
       await newSignature.save();
   
       return res.status(201).json({ message: 'Signature File uploaded successfully' });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Internal Server Error' });
+    }
+  });
+
+// to delete signatures
+router.delete('/deleteSignature/:id', async (req, res) => {
+    try {
+      const signature = await Signature.findById(req.params.id);
+      if (!signature) {
+        return res.status(404).json({ message: 'signature not found' });
+      }
+      await signature.deleteOne();
+      return res.status(200).json({ message: 'signature deleted successfully' });
     } catch (error) {
       console.error(error);
       return res.status(500).json({ message: 'Internal Server Error' });
